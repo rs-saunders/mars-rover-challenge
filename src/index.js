54729f4564fbb70036bb7add0c1a030d01190e76
Richard Saunders <richard@sdevsolutions.com>
2016-10-04 20:56:16 +0100

    npm start meets requirements
    - mission manually constructed
    - used sample data in problem
    - outputs correct for given inputs
    - roughly 4 hours of coding to this point

--- src/index.js
+++ src/index.js
@@ -2,27 +2,38 @@
 import Rover from './Rover';
 import Grid from './Grid';
 
-// sample input 1
+// ### Sample Input
+
 //5 3
 const grid = new Grid(5, 3);
+
 function commsLink(rover) {
     return !grid.isOffGrid(rover);
+}
+
+function sensorArray(rover, instruction) {
+    return grid.detectScent(rover, instruction);
 }
 
 //1 1 E
 //RFRFRFRF
 const rover = new Rover(1, 1, 'E');
-rover.executeInstructions('RFRFRFRF', commsLink);
-console.log(rover.toString());
+rover.executeInstructions('RFRFRFRF', commsLink, sensorArray);
 
 // 3 2 N
 // FRRFLLFFRRFLL
 const rover2 = new Rover(3, 2, 'N');
-rover2.executeInstructions('FRRFLLFFRRFLL', commsLink);
-console.log(rover2.toString());
+rover2.executeInstructions('FRRFLLFFRRFLL', commsLink, sensorArray);
 
 // 0 3 W
 // LLFFFLFLFL
 const rover3 = new Rover(0, 3, 'W');
-rover3.executeInstructions('LLFFFLFLFL', commsLink);
+rover3.executeInstructions('LLFFFLFLFL', commsLink, sensorArray);
+
+// ### Sample Output
+// 1 1 E
+// 3 3 N LOST
+// 2 3 S
+console.log(rover.toString());
+console.log(rover2.toString());
 console.log(rover3.toString());
