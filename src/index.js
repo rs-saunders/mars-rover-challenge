7cdb35168b2dc771e3eb68ff3ad33fa8675b1fcb
Richard Saunders <richard@sdevsolutions.com>
2016-10-04 19:30:53 +0100

    Manual construction of mission in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 /* eslint-disable no-console */
 import Rover from './Rover';
+import Grid from './Grid';
 
-const rover = new Rover();
+// sample input 1
+//5 3
+const grid = new Grid(5, 3);
+function commsLink(rover) {
+    return !grid.isOffGrid(rover);
+}
 
-console.log('Rover current position:', rover.toString());
+//1 1 E
+//RFRFRFRF
+const rover = new Rover(1, 1, 'E');
+rover.executeInstructions('RFRFRFRF', commsLink);
+console.log(rover.toString());
+
+// 3 2 N
+// FRRFLLFFRRFLL
+const rover2 = new Rover(3, 2, 'N');
+rover2.executeInstructions('FRRFLLFFRRFLL', commsLink);
+console.log(rover2.toString());
+
+// 0 3 W
+// LLFFFLFLFL
+const rover3 = new Rover(0, 3, 'W');
+rover3.executeInstructions('LLFFFLFLFL', commsLink);
+console.log(rover3.toString());
